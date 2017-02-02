3563447cd97707f6e19878e37756d3fb25a32d8e
NedM <Ned@TheLevelUp.com>
2017-02-02 14:17:41 -0500

    Exploring basic Ruby
    * Saying hello to me instead of world
    * Exploring mutator methods (bang!)
    * Adding detail regarding mutation of value based on method call

--- HelloWorld.rb
+++ HelloWorld.rb
@@ -1,1 +1,14 @@
-puts "Hello, world!"
+print "Who are you? "
+my_name = gets.chomp
+
+# Assigning result to new variable
+capitalized = my_name.capitalize
+
+puts "Original input: #{my_name}" 
+puts "#{capitalized} should be properly capitalized"
+
+# Altering the value of my_name!
+my_name.capitalize! #bang!
+puts "#{my_name} should now be properly capitalized"
+
+puts "Hello, #{my_name.capitalize}!"
